0540e8c39f17de8a312988dd68b37e3bd641094a
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-11-15 19:41:29 -0600

    an optional getaround

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -4,12 +4,13 @@ import ReactPlayer from "react-player"
 import "../css/VideoPlayer.css"
 import Home from "../pages/Home";
 import Clappr from  "@clappr/player"
+import { useStreamContext, useStreamUpdateContext } from "../context/StreamContext";
 
 
 
 export default function VideoPlayer(props) {
-  console.log(props.sources[0].src)
-
+  const getStreamName = useStreamContext();
+  console.log(getStreamName, props.sources[0].src)
     const videoNode = useRef(null);
     const [player, setPlayer] = useState(null);
     useEffect(() => {
@@ -25,7 +26,16 @@ export default function VideoPlayer(props) {
     }, []);
 
     useEffect(() => {
-      console.log(props)
+     const originalString = props.sources[0].src
+     const withoutPrefix = originalString.replace("https://video5.mayapur.tv/", "")
+
+     const withoutSuffix = withoutPrefix.replace("/chunklist.m3u8", "")
+      
+     if(getStreamName.stream == withoutSuffix){
+      return
+     } else {
+      window.location.reload()
+     }
     }, [props]);
   
     return (
